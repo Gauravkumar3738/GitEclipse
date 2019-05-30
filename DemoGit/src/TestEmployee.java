5fce037a341b98b311576cf0171d8e107e6ae3d9
gauravkumar3738 <gauravkumar3738@gmail.com>
2019-05-30 04:22:49 -0700

    main method

---
+++ DemoGit/src/TestEmployee.java
@@ -0,0 +1,16 @@
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		
+		Employee e1=new Employee();
+		
+		e1.setEmpId(101);
+		e1.setEmpName("Ramesh");
+		e1.setEmpSal(1000);
+		
+		
+
+	}
+
+}
